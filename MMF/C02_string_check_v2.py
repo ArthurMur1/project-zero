e96b3b2f31b7bdf704daa092345da70a132ad50e
ArthurMur1 <23235@fraser.school.nz>
2025-04-08 10:10:39 +1200

    added paymethod tab thing
    eya

---
+++ MMF/C02_string_check_v2.py
@@ -0,0 +1,34 @@
+def string_check(question, valid_ans_list=['yes','no'],
+                 num_letters=1):
+    """Checks if the users enter the full word or the 'n' letters/s of a word from a list of valid responses """
+
+
+    while True:
+
+        response = input (question).lower()
+
+        for item in valid_ans_list:
+
+            #check if response is the whole word
+            if response == item:
+                return item
+            
+            #check if its the first letter
+            elif response == item[ :num_letters]:
+                return item
+                        
+        print (f"Please choose an option from {valid_ans_list}")
+
+
+#main routine here
+payment_list = ['cash','credit']
+
+want_instructions = string_check ("Do you want to see the instructions? ")
+print (f"You chose {want_instructions}")
+pay_method = string_check (question= "Payment Method: ", payment_list, num_letters=2)
+print (f"You chose {pay_method}")
+
+
+
+
+    
