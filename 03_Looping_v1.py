448aa6b7dcdaf882e7f000a23ef0aed6c75ecbf4
ArthurMur1 <23235@fraser.school.nz>
2025-04-10 13:07:45 +1200

    name loop
    its not wroking

--- 03_Looping_v1.py
+++ 03_Looping_v1.py
@@ -1,3 +1,24 @@
-valid_names = ['A','B','C','D','E']
-while True:
-    print ("Name: ")
+#functions here
+question = "Name? "
+def name (question):
+#checks the user adds a yes no answerA
+
+ while True:
+    response = input(question).lower()
+
+    if response == "a":
+      return "A" 
+    elif response == "b":
+      return "B"
+    elif response == "c":
+      return "C" 
+    elif  response == "d":
+      return "D"
+    elif response == "xxx":
+     break  
+    else:
+      print ("You have sold {response} / 5 \n")
+    
+
+
+print(name(question))
--- C_03_instructions.py
+++ C_03_instructions.py
@@ -8,7 +8,26 @@ def make_statement(statement, decoration):
 
 def string_check(question, valid_answers=('yes','no'),
                 num_letters=1):
-    
+    def string_check(question, valid_ans_list, num_letters):
+    """Checks if the users enter the full word or the first letter of a word from a list of valid responses"""
+
+
+    while True:
+
+        response = input (question).lower()
+
+        for item in valid_ans_list:
+
+            #check if response is the whole word
+            if response == item:
+                return item
+            
+            #check if its the 'n' letters
+            elif response == item[:num_letters]:
+                return item
+            
+        print (f"Please choose an option from {valid_ans_list}")
+
 
 def instructions():
 make_statement(statement="Instrutions",decoration="ℹ️")
