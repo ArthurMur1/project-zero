176d84e41dda2939e071044a075b385d07f464ea
ArthurMur1 <23235@fraser.school.nz>
2025-04-11 14:39:16 +1200

    response made it work a little better
    it still dosent work

--- 03_Looping_v1.py
+++ 03_Looping_v1.py
@@ -1,24 +1,18 @@
 #functions here
+check = ['A','B','C','D','E']
 question = "Name? "
 def name (question):
+ response_amount = '5'
 #checks the user adds a yes no answerA
 
  while True:
     response = input(question).lower()
 
-    if response == "a":
-      return "A" 
-    elif response == "b":
-      return "B"
-    elif response == "c":
-      return "C" 
-    elif  response == "d":
-      return "D"
-    elif response == "xxx":
-     break  
-    else:
-      print ("You have sold {response} / 5 \n")
-    
+    if response == "a" or response == "b" or response == "c" or response == "d":
+      response = response_amount
+      print ("You have sold" + {response_amount} + " a/ 5 \n")
+      for item in check:
+        print ( response + " / 5")
 
 
 print(name(question))
