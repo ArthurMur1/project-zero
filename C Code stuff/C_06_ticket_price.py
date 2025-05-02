ce11a17d92efe94a8b9a2aaaa2678bce451fee84
ArthurMur1 <23235@fraser.school.nz>
2025-05-02 14:48:18 +1200

    it wors!!!!!!
    ticket prices

--- C Code stuff/C_06_ticket_price.py
+++ C Code stuff/C_06_ticket_price.py
@@ -29,6 +29,10 @@ def num_check(question, num_type, exit_code=None):
         except ValueError:
             print (error)
 
+
+
+
+
 def not_blank (question):
     """Checks the users answer isnt blank"""
 
@@ -39,21 +43,23 @@ def not_blank (question):
             return response
         
         print ("sorry this can't be blank. Please try again.\n")
-    #main routine starts here
-        who = not_blank ("please enter your name: ")
-        print (f"hello {who}")
 
 
-def string_check(question, valid_answers=('yes','no'),
-                 num_letters=1):
-    """Checks if the users enter the full word or the 'n' letters/s of a word from a list of valid responses """
+
+#main routine starts here
+who = not_blank ("please enter your name: ")
+print (f"hello {who}")
+
+
+def string_check(question, valid_ans_list, num_letters):
+    """Checks if the users enter the full word"""
 
 
     while True:
 
         response = input (question).lower()
 
-        for item in valid_answers:
+        for item in valid_ans_list:
 
             #check if response is the whole word
             if response == item:
@@ -66,79 +72,26 @@ def string_check(question, valid_answers=('yes','no'),
         print (f"Please choose an option from {valid_ans_list}")
 
 
-#initialise variables / non default options for string checker
-payment_ans = ('cash','credit')
-#loop for test
-while True:
-    print()
+#main routine here
+yes_no_list = ['yes', 'no']
+payment_list = ['cash','credit']
 
-    #ask for their name (and check its not blank)
-    name = not_blank ("Name: ")
+like_coffee = string_check (question="Age? A ", valid_ans_list=['yes', 'no'], num_letters=1 )
 
-    #ask for their age and check its berween 12 and 120
-    age = num_check ("Age: ")
+print (f"You chose {like_coffee}")
+pay_method = string_check (question= "Payment Method: ", valid_ans_list=['card', 'cash'],num_letters=2)
+print (f"You chose {pay_method}")
 
-    #output error message / success message
-    if age < 12:
-        print(f"{name} is too young ")
-        continue
-    elif age > 120:
-        print (f"{name} is too old")
-        continue
-    else:
-        pass
+payment_ans = ('cash', 'credit')
 
-    # ask user for the payment method (cash / credit / ca / cr)
-    pay_method = string_check (question= "Payment Method: ", payment_ans, num_letters=2)
-    print (f"{name} Has bought a ticket ({pay_method})")
+#ticket price list
+CHILD_PRICE = 7.50
+ADULT_PRICE = 10.50
+SENIOR_PRICE = 6.50
 
-#initialise ticket numbers
-    payment_ans = ('cash', 'credit')
+#CREDIT CARD SURCHARGE (currently 5%)
+CREDIT_SURCHARGE = 0.5
 
-    #Ticket Price List
-    CHILD_PRICE = 7.50
-    ADULT_PRICE = 10.50
-    SENIOR_PRICE = 6.50
+#loop for testing
 
-    # credit card surcharge (currently 5%)
-    CREDIT_SURCHARGE = 0.05
-
-    #loop for testing purposes...
-    while True:
-        print()
-        if age < 12:
-            print (f"{name} is too young")
-            continue
-
-        #child ticket price is $7.50
-        elif 12 <= age < 16:
-            ticket_price = CHILD_PRICE
-
-        #adult ticket price is ($10.50)
-        elif 16 <= age < 65:
-            ticket_price = ADULT_PRICE
-
-        #senior citizen ticket ($6.50)
-        elif 65 <= age < 121:
-            ticket_price = SENIOR_PRICE
-
-        else:
-            print(f"{name} is too old")
-            continue
-
-        #ask user for paymrnt method (cash / credit / ca / cr)
-        if pay_method == "cash":
-            surcharge = 0
-
-        #if paying by credit, calculate surcharge
-        else:
-            surcharge = ticket_price * CREDIT_SURCHARGE
-
-    #calculate total payable...
-        total_to_pay = ticket_price + surcharge
-
-        print (f"{name}'s ticket cost ${ticket_price:.2f}, they paid by {pay_method} "
-            f"so the surcharge is ${surcharge:.2f}/n"
-            f"The total payable is ${total_to_pay:.2f}/n")
-    
-    
+#coming back to this later
